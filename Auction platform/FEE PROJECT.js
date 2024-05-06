f89c4434563bfeff933382de907c39a586480e06
Vivekbhatt83073@gmail.com <vivekbhatt83073@gmail.com>
2024-05-06 07:37:35 +0530

    main nav added

---
+++ Auction platform/FEE PROJECT.js
@@ -0,0 +1,11 @@
+function showPopup() {
+    const menuPopup = document.getElementById('menu-popup');
+
+    if (menuPopup.style.display === 'none') {
+      menuPopup.style.display = 'block';
+    } else {
+      menuPopup.style.display = 'none';
+    }
+}
+
+
